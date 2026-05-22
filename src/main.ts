22197461de4f219796b1c69ea3e56cd786424750
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-05-21 23:21:16 -0300

    feat: add server config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,44 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import cookieParser from "cookie-parser";
+import { AppModule } from "./app.module";
+import { HttpExceptionFilter } from "./common/filters/http-exception.filter";
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+	const app = await NestFactory.create(AppModule);
+
+	const configService = app.get(ConfigService);
+
+	app.use(cookieParser());
+
+	app.setGlobalPrefix("api");
+
+	app.useGlobalPipes(
+		new ValidationPipe({
+			whitelist: true,
+			forbidNonWhitelisted: true,
+			transform: true,
+		}),
+	);
+
+	app.useGlobalFilters(new HttpExceptionFilter());
+
+	const config = new DocumentBuilder()
+		.setTitle("NestJS Auth API")
+		.setDescription("API documentation for NestJS Auth")
+		.setVersion("1.0")
+		.addBearerAuth()
+		.build();
+
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup("api/docs", app, document);
+
+	const port = configService.get<number>("PORT") ?? 3000;
+
+	await app.listen(port);
+	console.log(`Application running on http://localhost:${port}/api`);
+	console.log(`Docs available on http://localhost:${port}/api/docs`);
 }
 bootstrap();
