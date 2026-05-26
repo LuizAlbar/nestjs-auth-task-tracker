548fa60d84ef0365a68281e522a3c90a3d0b84db
LuizAlbar <luizgustavo.ab@hotmail.com>
2026-05-26 18:27:03 -0300

    feat: add users module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { UsersModule } from "./users/users.module";
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true,
-      expandVariables: true,
-      
+			expandVariables: true,
 		}),
+		UsersModule,
 	],
 })
 export class AppModule {}
